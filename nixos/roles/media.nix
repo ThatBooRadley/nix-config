89f2361941485b394d4c8d0ba9812169eaadef80
Bailey <bbignall1225@gmail.com>
2025-06-19 13:56:08 -0500

    added gabutdm

--- nixos/roles/media.nix
+++ nixos/roles/media.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [libsForQt5.kdenlive pixelorama jellyfin-media-player tailscale];
+  environment.systemPackages = with pkgs; [gabutdm libsForQt5.kdenlive pixelorama jellyfin-media-player tailscale];
   programs.obs-studio.enable = true;
   services.tailscale.enable = true;
 }
