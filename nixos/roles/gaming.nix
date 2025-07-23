50e80a0e6b6c842c189dc973c67dd71ec71a7bd6
Bailey <bbignall1225@gmail.com>
2025-07-23 18:05:16 -0500

    replaced rare with heroic

--- nixos/roles/gaming.nix
+++ nixos/roles/gaming.nix
@@ -6,7 +6,7 @@
     };
     gamemode.enable = true;
   };
-  environment.systemPackages = with pkgs; [openseeface itch prismlauncher protonup rare];
+  environment.systemPackages = with pkgs; [openseeface itch prismlauncher protonup heroic];
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/baileyb/.steam/root/compatibilitytools.d";
   };
