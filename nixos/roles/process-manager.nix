7871570958a06249284a783cb6af42da6a3cfa54
Bailey <bbignall1225@gmail.com>
2026-06-28 21:24:46 -0500

    replaced ventoy with mediawriter

--- nixos/roles/process-manager.nix
+++ nixos/roles/process-manager.nix
@@ -1,4 +1,4 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [czkawka resources bottles lufus];
+  environment.systemPackages = with pkgs; [czkawka resources bottles mediawriter];
   services.udisks2.enable = true;
 }
