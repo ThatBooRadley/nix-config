77521b2246bbb645479124b2f2bebfa4fca6f4fc
Bailey <bbignall1225@gmail.com>
2026-06-28 21:17:47 -0500

    added lufus

--- nixos/roles/process-manager.nix
+++ nixos/roles/process-manager.nix
@@ -1,4 +1,4 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [czkawka resources bottles];
+  environment.systemPackages = with pkgs; [czkawka resources bottles lufus];
   services.udisks2.enable = true;
 }
