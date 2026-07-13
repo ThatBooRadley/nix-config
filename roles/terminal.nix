6f5f11c201cd88e1d0f75d3fad9fd83b578e4869
ThatBooRadley <bbignall1225@gmail.com>
2026-07-12 19:31:15 -0500

    configured yazi

--- roles/terminal.nix
+++ roles/terminal.nix
@@ -6,6 +6,7 @@
     nushell
     zellij
     mediainfo
+    ouch
   ];
 
   fonts.packages = with pkgs; [
@@ -18,13 +19,7 @@
       yazi = {
         mgr = {
           show_hidden = true;
-          linemode = "size";
-          prepend_keymap = [
-            {on = ["y"]; run = ["yank" "plugin clipboard -- --action=copy"];} #clipboard
-            {on = ["c" "a" "a"]; run = "plugin compress"; desc = "Archive selected files";} #compress
-            {on = ["M"]; run = "plugin mount";} #mount
-            {on = ["R" "b"]; run = "plugin recycle-bin";} #recycle-bin
-          ];
+          linemode = "size"; 
         };
         plugin = {
           prepend_fetchers = [
@@ -43,10 +38,31 @@
         };
         tasks.image_alloc = 1024*1024*1024;
       };
+      keymap.mgr = {
+        prepend_keymap = [
+          {on = "y"; run = ["yank" "plugin clipboard -- --action=copy"];} #clipboard
+          {on = ["c" "a" "a"]; run = "plugin compress"; desc = "Archive selected files";} #compress
+          {on = "M"; run = "plugin mount";} #mount
+          {on = ["." "s"]; run = "plugin what-size"; desc = "Calc size of selection or cwd";} #what-size
+          {on = "o"; run = "plugin confirm-open"; desc = "Open selected files";} #confirm-open
+          {on = "<Enter>"; run = "plugin confirm-open"; desc = "Open selected files";} #confirm-open
+        ];
+      };
     };
     plugins = {
       inherit (pkgs.yaziPlugins) clipboard compress full-border git mount mediainfo ouch starship;
-      # localsend = pkgs."https://github.com/pakhromov/localsend.yazi";
+      what-size = pkgs.fetchFromGitHub {
+        owner = "pirafrank";
+        repo = "what-size.yazi";
+        rev = "179ebf69c9c3ade40cacc0f25e9557a43427c6ca";
+        sha256 = "7q/45TopqbojNRvYDmP9+hgSGPmiyLHBcV051qpOB2Y=";
+      };
+      confirm-open = pkgs.fetchFromGitHub {
+        owner = "walldmtd";
+        repo = "confirm-open.yazi";
+        rev = "058b9166fb4279fa5fcf5f11d3883bd94c3c937d";
+        sha256 = "AvY7O5XKD0M0W6bd9ah3aY4wj32E910uCYSFrkmxZoo=";
+      };
     };
     initLua = pkgs.writeText "init.lua" ''
       require("full-border"):setup {
@@ -56,6 +72,9 @@
         order = 1500,
       }
       require("starship"):setup()
+      require("confirm-open"):setup({
+        threshold = 2 -- Change as desired
+      })
     '';
   };
 
